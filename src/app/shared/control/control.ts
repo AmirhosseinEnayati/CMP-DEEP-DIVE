5183775bdc7d45267b245409afcfb58ac9e66778
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-26 16:31:55 +0330

    Transfer new-ticket's style to itself

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: false,
   templateUrl: './control.html',
   styleUrl: './control.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class Control {
   @Input({ required: true }) label!: string;
