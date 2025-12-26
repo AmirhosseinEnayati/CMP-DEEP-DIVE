623cad0e20b57fa67ab1275784bbb9bdcce65d82
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-26 17:32:11 +0330

    Access host element from its component

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, inject, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,8 +12,10 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   },
 })
 export class Control {
+  @Input({ required: true }) label!: string;
+  el = inject(ElementRef);
   onClick() {
     console.log('clicked');
+    console.log(this.el);
   }
-  @Input({ required: true }) label!: string;
 }
