0e8038e8f89dcebb6b893b191e72e31c0b6278d7
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-26 17:01:22 +0330

    Separate styles and remove extra elements by using :host

--- src/app/shared/button/button.ts
+++ src/app/shared/button/button.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: false,
   templateUrl: './button.html',
   styleUrl: './button.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class Button {}
