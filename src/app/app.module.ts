0f66496aa8d04d8a589835ba9e0be00fad48285e
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-26 12:13:39 +0330

    Add dashboard-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { App } from './app';
 import { BrowserModule } from '@angular/platform-browser';
 import { Header } from './header/header';
+import { DashboardItem } from './dashboard/dashboard-item/dashboard-item';
 
 @NgModule({
-  declarations: [App, Header],
+  declarations: [App, Header, DashboardItem],
   bootstrap: [App],
   imports: [BrowserModule],
 })
